f40b06d2f48ceeb0391ef0ad41f37fc74d67c7c7
Leeka99 <dsrn0000@gmail.com>
2025-08-12 16:33:54 +0900

    ♻️ refactor: 서비스 주소 변경

--- src/components/election/WaitingVotePage.tsx
+++ src/components/election/WaitingVotePage.tsx
@@ -37,7 +37,7 @@ const WaitingVotePage = () => {
   } | null>(null);
   const { shareWithTemplate } = useKakaoShare();
   // const url = `https://www.ittaeok.com/schedule/${scheduleId}/election/start-point`;
-  const url = `http://34.64.215.22/${scheduleId}/election/start-point`;
+  const url = `https://ittaeok-lkh.vercel.app/${scheduleId}/election/start-point`;
   const shareClickHandler = () => {
     shareWithTemplate(
       "내 출발 장소를 등록하고 모임 중간 장소를 찾아볼까요?",
