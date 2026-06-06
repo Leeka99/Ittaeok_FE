c8f8a595453977f0c605b0947a2abed0c152e0ec
Kanghyeon Lee <139203708+Leeka99@users.noreply.github.com>
2026-06-06 22:50:21 +0900

    Update useKakaoShare.ts

--- src/lib/api/useKakaoShare.ts
+++ src/lib/api/useKakaoShare.ts
@@ -1,7 +1,7 @@
 export const useKakaoShare = () => {
   const shareWithTemplate = (description: string, url = "") => {
     window.Kakao.Share.sendCustom({
-      templateId: 1282534,
+      templateId: 134037,
       templateArgs: {
         description,
         url,
